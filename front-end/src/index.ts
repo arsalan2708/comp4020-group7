b5718cf65b09ddef6d128351a1956ca1994ede20
charlesC <charles.chu9@gmail.com>
2025-03-11 01:10:44 -0500

    cleaned up imports

--- front-end/src/index.ts
+++ front-end/src/index.ts
@@ -1,21 +1,9 @@
+import { createIconButton } from "./components/icon-button.js";
+
 const tempBody = document.getElementById("body");
 
 if (tempBody) {
   //create button and add styling
   const component = createIconButton("/public/hamburgerIcon.png");
   tempBody.appendChild(component);
-}
-
-function createIconButton(src: string) {
-  //create button and add styling
-  const iconButton = document.createElement("button");
-  iconButton.classList.add("icon-button", "border-radius", "center");
-
-  // create image component, add styling and add to button
-  const icon = document.createElement("img");
-  icon.src = src;
-  icon.classList.add("icon-button__icon");
-  iconButton.appendChild(icon);
-
-  return iconButton;
 }
