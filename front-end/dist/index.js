dafde15318454956bf19184898bf5de2ae3e7bb6
charlesC <charles.chu9@gmail.com>
2025-03-11 07:41:55 -0500

    abstracted away page wrapper

--- front-end/dist/index.js
+++ front-end/dist/index.js
@@ -1,12 +1,9 @@
-import { mountAddButton } from "./components/add-button.js";
-import { mountNavBar } from "./components/nav-bar.js";
+import { mountPageWrapper } from "./components/page-wrapper.js";
 const IS_INDEX_PAGE = true;
-// create the top nav bar
-mountNavBar({ title: "Shared List", isIndexPage: IS_INDEX_PAGE });
-// mount action button
-mountAddButton({
+// mount page wrapper
+mountPageWrapper({
+    title: "Shared List",
     isIndexPage: IS_INDEX_PAGE,
-    //   TODO: action
     onAddClick: () => {
         const a = document.createElement("a");
         a.href = "/list.html";
