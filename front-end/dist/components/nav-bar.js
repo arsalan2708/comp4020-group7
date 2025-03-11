7178a712d5f11470c55402a54e65c5d9e0e213a4
charlesC <charles.chu9@gmail.com>
2025-03-11 04:26:51 -0500

    open animation

--- front-end/dist/components/nav-bar.js
+++ front-end/dist/components/nav-bar.js
@@ -1,10 +1,10 @@
 import { createIconButton } from "./icon-button.js";
-const IS_INDEX_PAGE = true;
 /**
  * populates the top navigation bar
  * @param title page title
+ * @param isIndexPage true meaning its being called for the index.html page
  */
-export function createNavBar(title) {
+export function createNavBar(title, isIndexPage) {
     const nav = document.querySelector(".page-wrapper__top-bar");
     if (nav) {
         // create heading and append it to nav
@@ -16,11 +16,11 @@ export function createNavBar(title) {
         const hamburgerIcon = createIconButton("../public/hamburgerIcon.png");
         hamburgerIcon.classList.add("page-wrapper__icon");
         // TODO: configure event handlers
-        hamburgerIcon.onclick = () => console.log("clicked");
+        hamburgerIcon.onclick = onModalOpen;
         nav.appendChild(hamburgerIcon);
         // TODO: finish side bar and its animations
         // create the side bar
-        // createSideBar(IS_INDEX_PAGE, "Sally");
+        // createSideBar(isIndexPage, "Sally");
     }
 }
 const sideBarItems = [
@@ -32,12 +32,14 @@ const sideBarItems = [
 ];
 /**
  *creates the side bar component and pairs it to the top bar
- * @param isHome true meaning its being called for the index.html page
+ * @param isIndexPage true meaning its being called for the index.html page
  */
-function createSideBar(isHome, userName) {
+function createSideBar(isIndexPage, userName) {
+    console.log("creating side bar");
+    const body = document.getElementById("body");
     // get modal
     const modal = document.createElement("div");
-    const body = document.getElementById("body");
+    modal.classList.add("modal");
     if (body && modal) {
         // append modal to body
         body.appendChild(modal);
@@ -49,7 +51,7 @@ function createSideBar(isHome, userName) {
         ul.classList.add("display-col");
         // add side bar items to list as li
         sideBarItems.forEach(({ label, displayHome }) => {
-            if ((isHome && displayHome) || !isHome) {
+            if ((isIndexPage && displayHome) || !isIndexPage) {
                 const li = document.createElement("li");
                 li.innerText = label;
                 ul.appendChild(li);
@@ -61,11 +63,35 @@ function createSideBar(isHome, userName) {
         username.classList.add("page-wrapper__username");
         // create aside and append list to it
         const aside = document.createElement("aside");
+        aside.id = "side-bar";
         aside.classList.add("height-full", "page-wrapper__side-bar");
         aside.appendChild(h2);
         aside.appendChild(ul);
         aside.appendChild(username);
         // append aside to modal window
         modal.appendChild(aside);
+    }
+}
+/**
+ * Event Handler for opening the side bar
+ */
+function onModalOpen() {
+    console.log("clicked");
+    //   mount the side ba component first
+    createSideBar(true, "Sally");
+    const sidebar = document.getElementById("side-bar");
+    if (sidebar) {
+        sidebar.classList.remove("side-bar--close");
+        sidebar.classList.add("side-bar--open");
+    }
+}
+/**
+ * Event Handler for closing the side bar
+ */
+function onModalClose() {
+    const sidebar = document.getElementById("side-bar");
+    if (sidebar) {
+        sidebar.classList.remove("side-bar--open");
+        sidebar.classList.add("side-bar--close");
     }
 }
--- front-end/src/components/nav-bar.ts
+++ front-end/src/components/nav-bar.ts
@@ -1,12 +1,11 @@
 import { createIconButton } from "./icon-button.js";
 
-const IS_INDEX_PAGE = true;
-
 /**
  * populates the top navigation bar
  * @param title page title
+ * @param isIndexPage true meaning its being called for the index.html page
  */
-export function createNavBar(title: string) {
+export function createNavBar(title: string, isIndexPage: boolean) {
   const nav = document.querySelector(".page-wrapper__top-bar");
 
   if (nav) {
@@ -20,12 +19,12 @@ export function createNavBar(title: string) {
     const hamburgerIcon = createIconButton("../public/hamburgerIcon.png");
     hamburgerIcon.classList.add("page-wrapper__icon");
     // TODO: configure event handlers
-    hamburgerIcon.onclick = () => console.log("clicked");
+    hamburgerIcon.onclick = onModalOpen;
     nav.appendChild(hamburgerIcon);
 
     // TODO: finish side bar and its animations
     // create the side bar
-    // createSideBar(IS_INDEX_PAGE, "Sally");
+    // createSideBar(isIndexPage, "Sally");
   }
 }
 
@@ -44,12 +43,15 @@ const sideBarItems: SideBarItemType[] = [
 
 /**
  *creates the side bar component and pairs it to the top bar
- * @param isHome true meaning its being called for the index.html page
+ * @param isIndexPage true meaning its being called for the index.html page
  */
-function createSideBar(isHome: boolean, userName: string) {
+function createSideBar(isIndexPage: boolean, userName: string) {
+  console.log("creating side bar");
+  const body = document.getElementById("body");
+
   // get modal
   const modal = document.createElement("div");
-  const body = document.getElementById("body");
+  modal.classList.add("modal");
 
   if (body && modal) {
     // append modal to body
@@ -65,7 +67,7 @@ function createSideBar(isHome: boolean, userName: string) {
 
     // add side bar items to list as li
     sideBarItems.forEach(({ label, displayHome }) => {
-      if ((isHome && displayHome) || !isHome) {
+      if ((isIndexPage && displayHome) || !isIndexPage) {
         const li = document.createElement("li");
         li.innerText = label;
         ul.appendChild(li);
@@ -79,6 +81,7 @@ function createSideBar(isHome: boolean, userName: string) {
 
     // create aside and append list to it
     const aside = document.createElement("aside");
+    aside.id = "side-bar";
     aside.classList.add("height-full", "page-wrapper__side-bar");
     aside.appendChild(h2);
     aside.appendChild(ul);
@@ -86,5 +89,33 @@ function createSideBar(isHome: boolean, userName: string) {
 
     // append aside to modal window
     modal.appendChild(aside);
+  }
+}
+
+/**
+ * Event Handler for opening the side bar
+ */
+function onModalOpen() {
+  console.log("clicked");
+
+  //   mount the side ba component first
+  createSideBar(true, "Sally");
+  const sidebar = document.getElementById("side-bar");
+
+  if (sidebar) {
+    sidebar.classList.remove("side-bar--close");
+    sidebar.classList.add("side-bar--open");
+  }
+}
+
+/**
+ * Event Handler for closing the side bar
+ */
+function onModalClose() {
+  const sidebar = document.getElementById("side-bar");
+
+  if (sidebar) {
+    sidebar.classList.remove("side-bar--open");
+    sidebar.classList.add("side-bar--close");
   }
 }
