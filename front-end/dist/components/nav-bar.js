df21cce9f7b45488f410ba085ff248e75284e1a1
charlesC <charles.chu9@gmail.com>
2025-03-11 03:10:00 -0500

    added side bar component. function ality missing still

--- front-end/dist/components/nav-bar.js
+++ front-end/dist/components/nav-bar.js
@@ -1,4 +1,5 @@
 import { createIconButton } from "./icon-button.js";
+const IS_INDEX_PAGE = true;
 /**
  * populates the top navigation bar
  * @param title page title
@@ -15,5 +16,51 @@ export function createNavBar(title) {
         const hamburgerIcon = createIconButton("../public/hamburgerIcon.png");
         hamburgerIcon.classList.add("page-wrapper__icon");
         nav.appendChild(hamburgerIcon);
+        // TODO: finish side bar and its animations
+        // create the side bar
+        // createSideBar(IS_INDEX_PAGE, "Sally");
+    }
+}
+const sideBarItems = [
+    { label: "Home", displayHome: false },
+    { label: "Edit Recurring Items", displayHome: true },
+    { label: "Edit Categories", displayHome: true },
+    { label: "Edit Participants", displayHome: false },
+    { label: "Notify Others", displayHome: false },
+];
+/**
+ *creates the side bar component and pairs it to the top bar
+ * @param isHome true meaning its being called for the index.html page
+ */
+function createSideBar(isHome, userName) {
+    // get modal
+    const modal = document.querySelector(".modal");
+    if (modal) {
+        // header text
+        const h2 = document.createElement("h2");
+        h2.innerText = "Options";
+        // create list
+        const ul = document.createElement("ul");
+        ul.classList.add("display-col");
+        // add side bar items to list as li
+        sideBarItems.forEach(({ label, displayHome }) => {
+            if ((isHome && displayHome) || !isHome) {
+                const li = document.createElement("li");
+                li.innerText = label;
+                ul.appendChild(li);
+            }
+        });
+        // user name
+        const username = document.createElement("h2");
+        username.innerText = userName;
+        username.classList.add("page-wrapper__username");
+        // create aside and append list to it
+        const aside = document.createElement("aside");
+        aside.classList.add("height-full", "page-wrapper__side-bar");
+        aside.appendChild(h2);
+        aside.appendChild(ul);
+        aside.appendChild(username);
+        // append aside to modal window
+        modal.appendChild(aside);
     }
 }
