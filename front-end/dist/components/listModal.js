f6cd9308639a0070970e1a5befaeb1764deac0ea
charlesC <charles.chu9@gmail.com>
2025-03-12 03:55:26 -0500

    inputs added

--- front-end/dist/components/listModal.js
+++ front-end/dist/components/listModal.js
@@ -1,10 +1,56 @@
-import { mountModalContainer } from "./modalContainer.js";
+import { createInput } from "../utils/createInput.js";
 export var ListModalMode;
 (function (ListModalMode) {
     ListModalMode[ListModalMode["Create"] = 0] = "Create";
     ListModalMode[ListModalMode["Edit"] = 1] = "Edit";
 })(ListModalMode || (ListModalMode = {}));
 export function mountListModal({ mode }) {
-    const modal = mountModalContainer({});
-    return modal;
+    // mount the modal and return it
+    //   const modal = mountModalContainer({});
+    //   modal?.classList.add("center");
+    //   stop if the modal mounting failed
+    //   if (!modal) return;
+    //   TODO: temporary remove this after figureing out form
+    const body = document.getElementById("body");
+    if (!body)
+        return;
+    //   title for the component
+    const title = document.createElement("p");
+    title.classList.add("listModal__title");
+    title.innerText = mode === ListModalMode.Create ? "Create List" : "Edit List";
+    //   create label input
+    const { inputNode: labelInput, container: labelContainer } = createInput({
+        id: "input-label",
+        label: "List Name",
+        name: "label",
+    });
+    //   input for list label additional options
+    labelInput.required = true;
+    labelInput.maxLength = 150;
+    labelInput.placeholder = "Enter List Name";
+    //   create label date
+    const { labelNode: dateLabel, inputNode: dateInput, container: dateContainer, } = createInput({
+        id: "input-date",
+        label: "Shopping Date",
+        name: "date",
+        type: "date",
+    });
+    //   add optional class
+    dateLabel.classList.add("label__optional");
+    //   input for list date additional options
+    dateInput.placeholder = "MM/DD/YYYY";
+    //   container for inputs
+    const inputContainer = document.createElement("div");
+    inputContainer.classList.add("listModal__inputContainer", "display-col", "border-radius");
+    inputContainer.append(labelContainer, dateContainer);
+    //   TODO: add recurring items
+    //   const recurringItemsContainer = document.createElement("div");
+    const buttonsContainer = document.createElement("div");
+    const form = document.createElement("form");
+    form.append(title, inputContainer, buttonsContainer);
+    form.classList.add("listModal", "border-radius", "display-col", "align--center");
+    //   append the form to the modal container
+    body.appendChild(form);
+    //   TODO: return modal
+    return form;
 }
--- front-end/src/components/listModal.ts
+++ front-end/src/components/listModal.ts
@@ -1,3 +1,4 @@
+import { createInput } from "../utils/createInput.js";
 import { mountModalContainer } from "./modalContainer.js";
 
 export enum ListModalMode {
@@ -10,7 +11,78 @@ interface ListModalProps {
 }
 
 export function mountListModal({ mode }: ListModalProps) {
-  const modal = mountModalContainer({});
+  // mount the modal and return it
+  //   const modal = mountModalContainer({});
+  //   modal?.classList.add("center");
 
-  return modal;
+  //   stop if the modal mounting failed
+  //   if (!modal) return;
+
+  //   TODO: temporary remove this after figureing out form
+  const body = document.getElementById("body");
+  if (!body) return;
+
+  //   title for the component
+  const title = document.createElement("p");
+  title.classList.add("listModal__title");
+  title.innerText = mode === ListModalMode.Create ? "Create List" : "Edit List";
+
+  //   create label input
+  const { inputNode: labelInput, container: labelContainer } = createInput({
+    id: "input-label",
+    label: "List Name",
+    name: "label",
+  });
+
+  //   input for list label additional options
+  labelInput.required = true;
+  labelInput.maxLength = 150;
+  labelInput.placeholder = "Enter List Name";
+
+  //   create label date
+  const {
+    labelNode: dateLabel,
+    inputNode: dateInput,
+    container: dateContainer,
+  } = createInput({
+    id: "input-date",
+    label: "Shopping Date",
+    name: "date",
+    type: "date",
+  });
+
+  //   add optional class
+  dateLabel.classList.add("label__optional");
+
+  //   input for list date additional options
+  dateInput.placeholder = "MM/DD/YYYY";
+
+  //   container for inputs
+  const inputContainer = document.createElement("div");
+  inputContainer.classList.add(
+    "listModal__inputContainer",
+    "display-col",
+    "border-radius"
+  );
+  inputContainer.append(labelContainer, dateContainer);
+
+  //   TODO: add recurring items
+  //   const recurringItemsContainer = document.createElement("div");
+
+  const buttonsContainer = document.createElement("div");
+
+  const form = document.createElement("form");
+  form.append(title, inputContainer, buttonsContainer);
+  form.classList.add(
+    "listModal",
+    "border-radius",
+    "display-col",
+    "align--center"
+  );
+
+  //   append the form to the modal container
+  body.appendChild(form);
+
+  //   TODO: return modal
+  return form;
 }
