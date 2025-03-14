8ff4b8ece7fcf82ea10ebffb2a90f037c685d494
charlesC <charles.chu9@gmail.com>
2025-03-14 05:56:02 -0500

    added long press feature

--- front-end/src/components/categoryFilter.ts
+++ front-end/src/components/categoryFilter.ts
@@ -1,5 +1,6 @@
 import { Categories } from "../types/types.js";
 import { addClasses } from "../utils/addClasses.js";
+import { onLongPress } from "../utils/longPress.js";
 import { mountRecurringItem } from "./recurringItem.js";
 
 interface Props {}
@@ -18,13 +19,31 @@ export function mountCategoryFilter() {
 
   //   container for the button to give sticky effect
   const optContainer = document.createElement("div");
-  addClasses(optContainer, "filter__buttonCont");
+  addClasses(optContainer, "filter__buttonCont", "center");
   optContainer.append(options);
 
   // component container
   const container = document.createElement("div");
   addClasses(container, "filter", "display-row", "align--center");
   container.append(optContainer);
+
+  //   long press event handler
+  onLongPress(container, () => {
+    addClasses(container, "filter--expanded");
+    addClasses(optContainer, "filter__buttonCont--expanded");
+  });
+
+  // close expanded form
+  document.addEventListener("touchstart", closeExpandedContainer);
+  document.addEventListener("click", closeExpandedContainer);
+
+  function closeExpandedContainer(ev: Event) {
+    const isExpenaded = container.classList.contains("filter--expanded");
+    if (!container.contains(ev.target as Node) && isExpenaded) {
+      container.classList.toggle("filter--expanded");
+      optContainer.classList.toggle("filter__buttonCont--expanded");
+    }
+  }
 
   //   add categories
   Categories.forEach((category) => {
