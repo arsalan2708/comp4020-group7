9e886d5f7fe5157e5b437e3444fc7a1af13a96e9
charlesC <charles.chu9@gmail.com>
2025-03-16 08:33:00 -0500

    type fixing

--- front-end/src/utils/crossTabEvent.ts
+++ front-end/src/utils/crossTabEvent.ts
@@ -1,6 +1,4 @@
-import { EventType } from "../types/types";
-
-export function sendCrossTabEvent(eventName: EventType, data: any) {
+export function sendCrossTabEvent(eventName: string, data: any) {
   localStorage.setItem(eventName, JSON.stringify(data));
 
   // Optional: Remove the item to avoid clutter
